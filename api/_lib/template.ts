b6c76934b439fdfa638bb3e099bf8c8818a21a46
Eric Ahmann <eric@ahmann.me>
2021-11-05 13:57:08 -0500

    indigo

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -44,7 +44,7 @@ function getCss(theme: string, fontSize: string) {
       }
 
     body {
-        background: ${background};
+        background: indigo;
         background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
         background-size: 100px 100px;
         height: 100vh;
